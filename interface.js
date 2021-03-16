0f751b4c51907ab17944fbe0bf026c842ad80444
Eds <steadyede@live.co.uk>
2021-03-16 17:53:41 +0000

    connected emojify api

--- interface.js
+++ interface.js
@@ -10,7 +10,6 @@ const testNotepad = new Notebook()
 ready(() => { 
   /* Do things after DOM has fully loaded */ 
   document.querySelector("#add-note").addEventListener("click", (e) => { /* ... */ 
-    // let note = document.querySelector(".add-note").text();
     e.preventDefault()
     let note = document.getElementById("note-submit").value;
     let testNote = new Note(note)
@@ -20,6 +19,15 @@ ready(() => {
     updateHTMLList()
 
   });
+
+  document.querySelector("#emojify-note").addEventListener("click", (e) => { 
+    e.preventDefault()
+    let text = document.getElementById("note-submit").value;
+    getEmojiData(text).then(response => response.json()).then(
+      res => document.querySelector("#emojified-text").textContent = res.emojified_text);
+
+  });
+  
 
 });
 
@@ -33,6 +41,18 @@ function updateHTMLList() {
     document.getElementById('view-notes').innerHTML += `<li><a href="/">${listItem}</a></li>`
 })
 }
+
+function getEmojiData(data) {
+  return fetch('https://makers-emojify.herokuapp.com/', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({text: data })
+  })
+}
+
+
 
 // $(document).ready(function() {
 //   let thermostat = new Thermostat();
