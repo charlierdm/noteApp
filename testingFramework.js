ffaabc72cc2e655663faa2e89b928e97e02230da
charlierdm <charlie.davismiller@gmail.com>
2021-03-16 11:11:13 +0000

    Added testingFramework file

---
+++ testingFramework.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function expect(a) {
+  return {
+    toEqual: function(b){
+
+      if(a === b)
+        console.log('Pass')
+      else
+        console.log('Fail')
+    }
+  }
+}
+
+function it(label, callback ) {
+  console.log(`Test: ${label}`)
+  callback()
+}
