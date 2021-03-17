df6de9897b83b3db4b297bbc624247e2d351e404
Eds <steadyede@live.co.uk>
2021-03-17 16:23:40 +0000

    static routing wip

--- interface.js
+++ interface.js
@@ -26,6 +26,25 @@ ready(() => {
 
   });
 
+  let notePage = "testing";
+
+  let routes = {
+    'notes'  : notePage, 
+    // 'view' : viewPage, 
+    // 'delete' : deletePage, 
+    // 'edit' : editPage
+  }
+
+
+
+  window.addEventListener('hashchange', (e) => {
+    let contentDiv = document.getElementById('right-side');
+    contentDiv.innerHTML = routes[window.location.hash.split('#')[1]];
+  })
+
+
+
+
 });
 
 function updateHTMLList() {
@@ -35,7 +54,7 @@ function updateHTMLList() {
   //add the notes to the html list
   testNotepad.viewNotes().forEach(function(note, index) {
     let listItem = note.displayNote()
-    document.getElementById('view-notes').innerHTML += `<li><a href="/">${listItem}...</a></li>`
+    document.getElementById('view-notes').innerHTML += `<li><a href="#notes">${listItem}...</a></li>`
 })
 }
 
@@ -52,10 +71,15 @@ function storeNotes() {
 function readStoredNotes() {
   // retrieve the string array of notes from local storage and turn into an array
   let notes = JSON.parse(localStorage.getItem("noteList"))
+  if (notes == null) {
+    notes = []
+    // create empty array if no local storage
+  }
 
   // add each note to the notepad
   notes.forEach(function(note) {
     testNotepad.addNote(new Note(note))
   })
 }
+
 
